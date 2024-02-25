3d613b49cd05ef88352b7ca2d890be545bd1461b
Leticia <leticiacoutolx@gmail.com>
2024-02-25 17:42:18 -0300

    Refactoring and creating the plans session

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Button from "./components/Button";
 import CardSimple from "./components/cardSimple";
 
 const titleStyle = "font-semibold text-zinc-800 leading-tight";
+const colorfulArticle =
+  "px-6 md:px-32 py-16 bg-blue-50 grid lg:grid-cols-2 place-items-center lg:gap-24";
 
 import illustrationMan from "/illustration-man-home-1.svg";
 import person from "/person.svg";
@@ -11,13 +13,14 @@ import happyEmoji from "/happy-emoji.svg";
 import playStoreButton from "/google-play.svg";
 import appStore from "/app-store.svg";
 import illustrationNotification from "/illustration-notifications.svg";
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <article className="px-6 md:px-32 py-16 bg-blue-50 grid lg:grid-cols-2 place-items-center lg:gap-24">
+        <article className={colorfulArticle}>
           <div>
             <h1 className={`${titleStyle} text-4xl sm:text-5xl mb-12`}>
               Rachi,
@@ -33,7 +36,9 @@ function App() {
         </article>
 
         <article className="flex flex-col place-items-center mt-14">
-          <h2 className={`${titleStyle} text-4xl mb-12`}>Como funciona</h2>
+          <h2 className={`${titleStyle} text-2xl sm:text-4xl`}>
+            Como funciona
+          </h2>
 
           <section className="flex flex-col gap-11 lg:flex-row place-items-center justify-between w-2/4 mt-20 mb-28">
             <CardSimple
@@ -59,13 +64,13 @@ function App() {
           </section>
         </article>
 
-        <article className="px-6 md:px-32 py-16 bg-blue-50 grid lg:grid-cols-2 place-items-center lg:gap-24">
+        <article className={colorfulArticle}>
           <div>
-            <h1 className={`${titleStyle} text-3xl sm:text-4xl mb-6`}>
+            <h2 className={`${titleStyle} text-3xl sm:text-4xl mb-6`}>
               Baixe nosso app para desfrutar mais!
-            </h1>
+            </h2>
 
-            <p className="text-zinc-600 max-w-[444px]">
+            <p className="text-zinc-600 text-sm sm:text-lg max-w-[444px]">
               Amet in elementum nulla scelerisque dui, egestas at. Elit
               consectetur turpis elementum amet vitae et etiam nec. Varius
               volutpat hac adipiscing tincidunt pretium.
@@ -89,6 +94,44 @@ function App() {
             src={illustrationNotification}
             alt="illustration Notifications"
           />
+        </article>
+
+        <article className="flex flex-col place-items-center mt-12 px-3 sm:px-6">
+          <h2 className={`${titleStyle} text-2xl sm:text-4xl mb-4`}>
+            Nossos Planos
+          </h2>
+
+          <p className="text-center text-sm sm:text-lg text-zinc-600">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            lacinia mi quis euismod ultrices.
+          </p>
+
+          <section className="flex flex-col lg:flex-row gap-8 mt-10">
+            <Card
+              title={"Bronze"}
+              price={28}
+              description={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia mi quis euismod ultrices."
+              }
+              isDark={false}
+            />
+            <Card
+              title={"Prata"}
+              price={57}
+              description={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia mi quis euismod ultrices."
+              }
+              isDark={true}
+            />
+            <Card
+              title={"Ouro"}
+              price={94}
+              description={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia mi quis euismod ultrices."
+              }
+              isDark={false}
+            />
+          </section>
         </article>
       </main>
     </>
