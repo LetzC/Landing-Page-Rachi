7a92cc5d937c9ad0eef5676316c1ac886c2992c5
Leticia <leticiacoutolx@gmail.com>
2024-02-25 15:43:23 -0300

    Header creation and styling

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,36 @@
+import logo from "../../../public/logo.svg";
+import { useMediaQuery } from "react-responsive";
+import MenuBurger from "./MenuBurger/index";
+
+const aStyle = "text-zinc-800 hover:text-blue-800 transition-colors";
+
+export default function Header() {
+  const isTablet = useMediaQuery({ query: "(max-width: 768px)" });
+
+  return (
+    <header className="flex justify-between py-5 px-6 md:px-32">
+      <a href="#">
+        <img src={logo} alt="Logo Rachi" className="w-20 md:w-100" />
+      </a>
+
+      {isTablet ? (
+        <MenuBurger />
+      ) : (
+        <nav className="flex gap-16">
+          <a href="#" className={aStyle}>
+            Funcionalidades
+          </a>
+          <a href="#" className={aStyle}>
+            App
+          </a>
+          <a href="#" className={aStyle}>
+            Planos
+          </a>
+          <a href="#" className={aStyle}>
+            Contato
+          </a>
+        </nav>
+      )}
+    </header>
+  );
+}
