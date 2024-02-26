79961a130b0c689178767334121b932b4613bca2
Leticia <leticiacoutolx@gmail.com>
2024-02-26 14:13:02 -0300

    Footer creation and styling

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p className="text-sm md:text-xl text-zinc-800 font-medium text-center py-5 md:py-8">
+        Rachi - Todos os direitos reservados.
+      </p>
+    </footer>
+  );
+}
